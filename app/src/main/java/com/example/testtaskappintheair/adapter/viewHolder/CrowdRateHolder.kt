6e202cc43f7bd02157e40ec02843775cec72d8df
ilyatim <31735653+ilyatim@users.noreply.github.com>
2020-11-24 22:09:56 +0300

    feat: crowded rating bar customization

---
+++ app/src/main/java/com/example/testtaskappintheair/adapter/viewHolder/CrowdRateHolder.kt
@@ -0,0 +1,37 @@
+package com.example.testtaskappintheair.adapter.viewHolder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.TextView
+import com.example.testtaskappintheair.R
+import com.example.testtaskappintheair.adapter.callback.OnRatingBarChangeCallback
+import com.example.testtaskappintheair.model.RecyclerViewCell
+
+class CrowdRateHolder(
+    layoutInflater: LayoutInflater,
+    parent: ViewGroup,
+    ratingChangeListener: OnRatingBarChangeCallback
+) : AbsViewHolder(layoutInflater.inflate(
+    R.layout.recycler_item_rate_crowd,
+    parent,
+    false
+)) {
+
+    private val titleTextView: TextView
+            = itemView.findViewById(R.id.submit_fragment_recycler_rating_text_view_crowd)
+    private val ratingBar: RatingBar
+            = itemView.findViewById(R.id.submit_fragment_recycler_rating_bar_crowd)
+
+    init {
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            ratingChangeListener.onRatingBarChange(adapterPosition, rating)
+        }
+    }
+
+    override fun bind(cell: RecyclerViewCell) {
+        val ratingCell = cell as RecyclerViewCell.CrowdRateDataClass
+        titleTextView.text = ratingCell.title
+        ratingBar.rating = ratingCell.rate.toFloat()
+    }
+}
